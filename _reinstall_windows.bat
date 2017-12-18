77ac67b9664433d1df8019427e1fdfb91dd9e8da
Erwan Normand <normand.erwan@protonmail.com>
2017-12-18 17:13:29 +0100

    Wait the software installation before calling the shell initialization

--- _reinstall_windows.bat
+++ _reinstall_windows.bat
@@ -4,6 +4,8 @@ echo "Installing softwares: starting 'softwares_installation.bat'"
 Powershell -Command "& { Start-Process \"%cd%\softwares_installation.bat\" -verb RunAs}"
 echo ""
 
+set /p x=Wait the softwares to install...
+
 echo "Configuring shell environment: starting 'shell_initialization.sh'"
 start "" "%PROGRAMFILES%\Git\bin\sh.exe" --login -i -- shell_initialization.sh
 
